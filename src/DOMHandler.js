5af8206790f33f376b9dbfc74ab7b535f1ae40dd
norraynor <alechanderr@gmail.com>
2021-12-06 20:02:20 +0100

    added visuals

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -2,17 +2,27 @@ function DOMHandler(gameboardSize){
     function createGameboard(type,owner){
         const gameboard = document.createElement("div");
         gameboard.className = "gameboard";
+        gameboard.owner = owner;
+        gameboard.type = type;
         for(let i = 0; i<gameboardSize;i++){
             for(let j = 0;j<gameboardSize;j++){
                 const gItem = document.createElement("div");
                 gItem.className = "gameboard-item";
                 gItem.id = ("" + i) + (j + "");
+                gItem.addEventListener("click",()=>{
+                    console.log({gItem,gameboard});
+                    //here it should mark hit and record it on gameboard
+                })
                 gameboard.appendChild(gItem);
             }
         }
         gameboard.type = type;
         gameboard.owner = owner;
         return gameboard;
+    }
+    function refreshGameboard(){
+        //after click should mark spot and tell gameboard that it has been hit
+        //refresh gameboard based on hits and ships
     }
     return {
         createGameboard
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import DOMHandler from './DOMHandler';
 function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
+    const container = document.createElement("div");
+    container.classList.add("container");
     
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -17,8 +19,15 @@ function component() {
 
 
     element.appendChild(btn);
-
-    //element.appendChild(DOMHandler(5).createGameboard("hit","mee"))
+    const shipGameboard = DOMHandler(5).createGameboard("ship","mee");
+    const hitGameboard = DOMHandler(5).createGameboard("hit","mee");
+    const cShipGameboard = DOMHandler(5).createGameboard("ship","enemy");
+    const cHitGameboard = DOMHandler(5).createGameboard("hit","enemy");
+    container.appendChild(shipGameboard);
+    container.appendChild(hitGameboard);
+    container.appendChild(cShipGameboard);
+    container.appendChild(cHitGameboard);
+    element.appendChild(container);
 
     return element;
   }
